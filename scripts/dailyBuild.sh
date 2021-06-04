774f8ea4b572b219b4a13ae3da3bb9e2876cedd9
Gilbert Kwan <gkwan@ca.ibm.com>
2021-06-04 11:37:40 -0400

    Revert "Merge staging to prod - Version update (#76)"

--- scripts/dailyBuild.sh
+++ scripts/dailyBuild.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
-while getopts t:d: flag;
+while getopts t:d:b:u: flag;
 do
     case "${flag}" in
         t) DATE="${OPTARG}";;
         d) DRIVER="${OPTARG}";;
-        *) echo "Invalid option";;
+        b) BUILD="${OPTARG}";;
+        u) DOCKER_USERNAME="${OPTARG}"
     esac
 done
 
-sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/$DATE/$DRIVER</runtimeUrl></install></configuration>" pom.xml
+sed -i "\#<artifactId>liberty-maven-plugin</artifactId>#a<configuration><install><runtimeUrl>https://public.dhe.ibm.com/ibmdl/export/pub/software/openliberty/runtime/nightly/"$DATE"/"$DRIVER"</runtimeUrl></install></configuration>" pom.xml
 cat pom.xml
 
 ../scripts/testApp.sh
