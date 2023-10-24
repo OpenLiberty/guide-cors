ea67942f2ecb70c65604e65ecc30f82121de2c79
saumyapandyaa <pandyasaumya26@gmail.com>
2023-10-24 17:37:44 -0400

    added scheme + updated syntax issues

--- finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
+++ finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
@@ -1,15 +1,14 @@
-// tag::comment[]
+// tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2019 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Contributors:
- *     IBM Corporation - Initial implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
-// end::comment[]
+// end::copyright[]
 package it.io.openliberty.guides.cors;
 
 import java.util.HashMap;
@@ -17,39 +16,68 @@ import java.util.Map;
 
 public class TestData {
 
-    public static String REQUEST_HEADER_ORIGIN = "Origin";
-    public static String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
-    public static String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    public final String REQUEST_HEADER_ORIGIN;
+    public final String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD;
+    public final String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS;
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS;
+    public final Map<String, String> simpleRequestHeaders;
+    public final Map<String, String> simpleResponseHeaders;
+    public final Map<String, String> preflightRequestHeaders;
+    public final Map<String, String> preflightResponseHeaders;
 
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
-    public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public final String prefix;
 
-    public static Map<String, String> simpleRequestHeaders = new HashMap<String, String>();
-    public static Map<String, String> simpleResponseHeaders = new HashMap<String, String>();
+    public TestData() {
 
-    public static Map<String, String> preflightRequestHeaders = new HashMap<String, String>();
-    public static Map<String, String> preflightResponseHeaders = new HashMap<String, String>();
+        prefix = "Access-Control-";
 
-    static {
-        simpleRequestHeaders.put(REQUEST_HEADER_ORIGIN, "openliberty.io");
+        REQUEST_HEADER_ORIGIN = "Origin";
+        REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD = prefix + "Request-Method";
+        REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS = prefix + "Request-Headers";
 
-        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "openliberty.io");
-        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS, "MyHeader");
+        RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = prefix + "Allow-Origin";
+        RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS = prefix + "Allow-Credentials";
+        RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS = prefix + "Expose-Headers";
+        RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE = prefix + "Max-Age";
+        RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS = prefix + "Allow-Methods";
+        RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS = prefix + "Allow-Headers";
+
+        simpleRequestHeaders = new HashMap<String, String>();
+        simpleResponseHeaders = new HashMap<String, String>();
+
+        preflightRequestHeaders = new HashMap<String, String>();
+        preflightResponseHeaders = new HashMap<String, String>();
+
+
+        simpleRequestHeaders.put(REQUEST_HEADER_ORIGIN, "http://openliberty.io");
+
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
+                "http://openliberty.io");
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                "true");
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS,
+                "MyHeader");
 
         preflightRequestHeaders.put(REQUEST_HEADER_ORIGIN, "anywebsiteyoulike.com");
-        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD, "DELETE");
-        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS, "MyOwnHeader2");
+        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD,
+                "DELETE");
+        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS,
+                "MyOwnHeader2");
 
-        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN, "anywebsiteyoulike.com");
-        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
+                "anywebsiteyoulike.com");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                "true");
         preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE, "10");
-        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS, "OPTIONS, DELETE");
-        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS, "MyOwnHeader1, MyOwnHeader2");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS,
+                "OPTIONS, DELETE");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS,
+                "MyOwnHeader1, MyOwnHeader2");
     }
 
 }
