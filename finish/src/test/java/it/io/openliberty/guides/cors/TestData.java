77c30e93dba42c09e062de13ff177ded140973f9
saumyapandyaa <pandyasaumya26@gmail.com>
2023-10-24 15:00:43 -0400

    changed type from static to final

--- finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
+++ finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
@@ -17,57 +17,48 @@ import java.util.Map;
 
 public class TestData {
 
-        public static String REQUEST_HEADER_ORIGIN = "Origin";
-        public static String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
-        public static String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    public final String REQUEST_HEADER_ORIGIN = "Origin";
+    public final String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
+    public final String REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
 
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
-        public static String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    public final String RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
 
-        public static Map<String, String> simpleRequestHeaders = new HashMap<String, String>();
-        public static Map<String, String> simpleResponseHeaders = new HashMap<String, String>();
+    public final Map<String, String> simpleRequestHeaders = new HashMap<String, String>();
+    public final Map<String, String> simpleResponseHeaders = new HashMap<String, String>();
 
-        public static Map<String, String> preflightRequestHeaders = new HashMap<String, String>();
-        public static Map<String, String> preflightResponseHeaders = new HashMap<String, String>();
+    public final Map<String, String> preflightRequestHeaders = new HashMap<String, String>();
+    public final Map<String, String> preflightResponseHeaders = new HashMap<String, String>();
 
-        static {
-                simpleRequestHeaders.put(REQUEST_HEADER_ORIGIN,
-                                "http://openliberty.io");
+    public TestData() {
+        simpleRequestHeaders.put(REQUEST_HEADER_ORIGIN, "http://openliberty.io");
 
-                simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
-                                "http://openliberty.io");
-                simpleResponseHeaders.put(
-                                RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
-                                "true");
-                simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS,
-                                "MyHeader");
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
+                "http://openliberty.io");
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                "true");
+        simpleResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_EXPOSE_HEADERS,
+                "MyHeader");
 
-                preflightRequestHeaders.put(REQUEST_HEADER_ORIGIN,
-                                "anywebsiteyoulike.com");
-                preflightRequestHeaders.put(
-                                REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD, "DELETE");
-                preflightRequestHeaders.put(
-                                REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS,
-                                "MyOwnHeader2");
+        preflightRequestHeaders.put(REQUEST_HEADER_ORIGIN, "anywebsiteyoulike.com");
+        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_METHOD,
+                "DELETE");
+        preflightRequestHeaders.put(REQUEST_HEADER_ACCESS_CONTROL_REQUEST_HEADERS,
+                "MyOwnHeader2");
 
-                preflightResponseHeaders.put(
-                                RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
-                                "anywebsiteyoulike.com");
-                preflightResponseHeaders.put(
-                                RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
-                                "true");
-                preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE,
-                                "10");
-                preflightResponseHeaders.put(
-                                RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS,
-                                "OPTIONS, DELETE");
-                preflightResponseHeaders.put(
-                                RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS,
-                                "MyOwnHeader1, MyOwnHeader2");
-        }
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_ORIGIN,
+                "anywebsiteyoulike.com");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_CREDENTIALS,
+                "true");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_MAX_AGE, "10");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_METHODS,
+                "OPTIONS, DELETE");
+        preflightResponseHeaders.put(RESPONSE_HEADER_ACCESS_CONTROL_ALLOW_HEADERS,
+                "MyOwnHeader1, MyOwnHeader2");
+    }
 
 }
