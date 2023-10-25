fa885936a7506910fc760d55acf1f93adc35511b
Gilbert Kwan <gkwan@ca.ibm.com>
2023-10-25 15:44:42 -0400

    dev content review

--- finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
+++ finish/src/test/java/it/io/openliberty/guides/cors/TestData.java
@@ -16,39 +16,39 @@ import java.util.Map;
 
 public class TestData {
 
-    public static final String ORIGIN = "Origin";
-    public static final String ACCESS_CONTROL_REQUEST_METHOD = "Access-Control-Request-Method";
-    public static final String ACCESS_CONTROL_REQUEST_HEADERS = "Access-Control-Request-Headers";
+    static final String ORIGIN = "Origin";
+    static final String AC_REQUEST_METHOD = "Access-Control-Request-Method";
+    static final String AC_REQUEST_HEADERS = "Access-Control-Request-Headers";
 
-    public static final String ACCESS_CONTROL_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
-    public static final String ACCESS_CONTROL_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
-    public static final String ACCESS_CONTROL_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
-    public static final String ACCESS_CONTROL_MAX_AGE = "Access-Control-Max-Age";
-    public static final String ACCESS_CONTROL_ALLOW_METHODS = "Access-Control-Allow-Methods";
-    public static final String ACCESS_CONTROL_ALLOW_HEADERS = "Access-Control-Allow-Headers";
+    static final String AC_ALLOW_ORIGIN = "Access-Control-Allow-Origin";
+    static final String AC_ALLOW_CREDENTIALS = "Access-Control-Allow-Credentials";
+    static final String AC_EXPOSE_HEADERS = "Access-Control-Expose-Headers";
+    static final String AC_MAX_AGE = "Access-Control-Max-Age";
+    static final String AC_ALLOW_METHODS = "Access-Control-Allow-Methods";
+    static final String AC_ALLOW_HEADERS = "Access-Control-Allow-Headers";
 
-    public static Map<String, String> simpleRequestHeaders = new HashMap<String, String>();
-    public static Map<String, String> simpleResponseHeaders = new HashMap<String, String>();
+    static Map<String,String> simpleRequestHeaders = new HashMap<String, String>();
+    static Map<String,String> simpleResponseHeaders = new HashMap<String, String>();
 
-    public static Map<String, String> preflightRequestHeaders = new HashMap<String, String>();
-    public static Map<String, String> preflightResponseHeaders = new HashMap<String, String>();
+    static Map<String,String> preflightRequestHeaders = new HashMap<String, String>();
+    static Map<String,String> preflightResponseHeaders = new HashMap<String, String>();
 
     static {
         simpleRequestHeaders.put(ORIGIN, "openliberty.io");
 
-        simpleResponseHeaders.put(ACCESS_CONTROL_ALLOW_ORIGIN, "http://openliberty.io");
-        simpleResponseHeaders.put(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        simpleResponseHeaders.put(ACCESS_CONTROL_EXPOSE_HEADERS, "MyHeader");
+        simpleResponseHeaders.put(AC_ALLOW_ORIGIN, "openliberty.io");
+        simpleResponseHeaders.put(AC_ALLOW_CREDENTIALS, "true");
+        simpleResponseHeaders.put(AC_EXPOSE_HEADERS, "MyHeader");
 
         preflightRequestHeaders.put(ORIGIN, "anywebsiteyoulike.com");
-        preflightRequestHeaders.put(ACCESS_CONTROL_REQUEST_METHOD, "DELETE");
-        preflightRequestHeaders.put(ACCESS_CONTROL_REQUEST_HEADERS, "MyOwnHeader2");
+        preflightRequestHeaders.put(AC_REQUEST_METHOD, "DELETE");
+        preflightRequestHeaders.put(AC_REQUEST_HEADERS, "MyOwnHeader2");
 
-        preflightResponseHeaders.put(ACCESS_CONTROL_ALLOW_ORIGIN, "anywebsiteyoulike.com");
-        preflightResponseHeaders.put(ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
-        preflightResponseHeaders.put(ACCESS_CONTROL_MAX_AGE, "10");
-        preflightResponseHeaders.put(ACCESS_CONTROL_ALLOW_METHODS, "OPTIONS, DELETE");
-        preflightResponseHeaders.put(ACCESS_CONTROL_ALLOW_HEADERS, "MyOwnHeader1, MyOwnHeader2");
+        preflightResponseHeaders.put(AC_ALLOW_ORIGIN, "anywebsiteyoulike.com");
+        preflightResponseHeaders.put(AC_ALLOW_CREDENTIALS, "true");
+        preflightResponseHeaders.put(AC_MAX_AGE, "10");
+        preflightResponseHeaders.put(AC_ALLOW_METHODS, "OPTIONS, DELETE");
+        preflightResponseHeaders.put(AC_ALLOW_HEADERS, "MyOwnHeader1, MyOwnHeader2");
     }
 
 }
