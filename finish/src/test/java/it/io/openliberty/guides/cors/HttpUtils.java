f9a27b7f2174c90c1d8caa5a6f80ffdf7f887efb
Gilbert Kwan <gkwan@ca.ibm.com>
2024-01-29 19:46:27 -0500

    Update HttpUtils.java

--- finish/src/test/java/it/io/openliberty/guides/cors/HttpUtils.java
+++ finish/src/test/java/it/io/openliberty/guides/cors/HttpUtils.java
@@ -1,15 +1,14 @@
-// tag::comment[]
+// tag::copyright[]
 /*******************************************************************************
- * Copyright (c) 2017 IBM Corporation and others.
+ * Copyright (c) 2017, 2024 IBM Corporation and others.
  * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * http://www.eclipse.org/legal/epl-2.0/
  *
- * Contributors:
- *     IBM Corporation - Initial implementation
+ * SPDX-License-Identifier: EPL-2.0
  *******************************************************************************/
-// end::comment[]
+// end::copyright[]
 package it.io.openliberty.guides.cors;
 
 import java.io.IOException;
